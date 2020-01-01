3da410e3799909956aca654d094c1b428ce801af
Deepak Brar <deepak.brar@jpl.nasa.gov>
2019-12-31 20:14:51 -0800

    more changes

--- scripts.js
+++ scripts.js
@@ -29,3 +29,16 @@ $(document).ready(function() {
     stickyToggle(sticky, stickyWrapper, $(window));
   });
 });
+
+$('.dropdown-submenu > a').on("click", function(e) {
+  var submenu = $(this);
+  $('.dropdown-submenu .dropdown-menu').removeClass('show');
+  submenu.next('.dropdown-menu').addClass('show');
+  e.stopPropagation();
+});
+
+$('.dropdown').on("hidden.bs.dropdown", function() {
+  // hide any open menus when parent closes
+  $('.dropdown-menu.show').removeClass('show');
+});
+
